b3e9a5e8fe185ef8da8533765ce9d8e03f1ed6ca
StevenNoty <steven88724@gmail.com>
2023-10-18 22:37:50 -0500

    Update

--- assets/js/cargarRegistro.js
+++ assets/js/cargarRegistro.js
@@ -75,6 +75,54 @@ function registrar() {
     contenedorRegistro.classList.remove('ocultar-registro');
     contenedorRegistro.innerHTML = plantilla;
 
+    const formulario = document.getElementById('form');
+
+    formulario.addEventListener('submit', (e) => {
+        e.preventDefault();
+        validarFormulario();
+    });
+
+    function validarFormulario() {
+        // Validar campo nombres
+        const nombres = document.getElementById('name').value;
+        if (nombres.length < 3) {
+            alert('El campo nombres debe tener al menos 3 caracteres.');
+            return false;
+        }
+
+        // Validar campo correo
+        const correo = document.getElementById('email').value;
+        const regexCorreo = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+        if (!regexCorreo.test(correo)) {
+            alert('El correo electrónico no es válido.');
+            return false;
+        }
+
+        // Validar campo número
+        const numero = document.getElementById('number').value;
+        const regexNumero = /^0?[0-9]{1,9}$/;
+        if (!regexNumero.test(numero)) {
+            alert('El número de teléfono no es válido.');
+            return false;
+        }
+
+        // Validar campo contraseña
+        const contraseña = document.getElementById('password').value;
+        const confirmarContraseña = document.getElementById('confirmpassword').value;
+        if (contraseña.length < 8) {
+            alert('La contraseña debe tener al menos 8 caracteres.');
+            return false;
+        } else if (contraseña !== confirmarContraseña) {
+            alert('Las contraseñas no coinciden.');
+            return false;
+        }
+        // Si todos los campos son válidos, se envía el formulario
+        alert("Gracias por registrarte.");
+        contenedorRegistro.classList.add('ocultar-registro');
+        // document.getElementById('form').reset();
+        return true;
+    }
+
     const cerrar = document.querySelectorAll('.cerrar');
 
     cerrar.forEach((item) => {
@@ -82,4 +130,4 @@ function registrar() {
             contenedorRegistro.classList.add('ocultar-registro');
         });
     });
-}
+};
